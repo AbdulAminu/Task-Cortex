adfe6adb03684e8b8dd33b1c4bed3147bf3ba652
AbdulAminu <abdulrasheedkola2002@gmail.com>
2026-05-17 10:45:40 +0100

    added changes

--- src/API/api.js
+++ src/API/api.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://e-xpress-backend.vercel.app",
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
